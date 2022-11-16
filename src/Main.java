15fc636dc746dd4e8cacec8fa1836d6eac8b3047
Ekaterina Parfinenko <katyavasilenko2012@gmail.com>
2022-11-16 09:18:36 +0300

    variables renamed

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         int amount = 20_000;
-        int mile = 20;
+        int bonusRatio = 20;
 
-        int bonus = amount / mile;
+        int bonusMile = amount / bonusRatio;
 
-        System.out.println((bonus) + " Бонус за купленный билет");
+        System.out.println((bonusMile) + " Бонус за купленный билет");
     }
 }
